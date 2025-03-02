f1d2f8e98e66bd86420a6f5dcd2a0241dc976201
Julián Laterza <laterzajulian@gmail.com>
2025-03-02 17:31:08 -0300

    iniciacion de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 // ignore_for_file: unused_import
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:testchatscreen/firebase_options.dart';
 import 'package:testchatscreen/pages/register_page.dart';
+import 'package:testchatscreen/services/auth/auth_gate.dart';
 import 'package:testchatscreen/services/auth/login_or_register.dart';
 import 'pages/login_page.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
